b54a6dabf9b97077ac9c18bcbdbf149fefb2f7c2
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-05 19:11:46 +0600

    innerHTML vs createElement

--- 06-document-object-model/08-create-item-innerHTML-vs-createElement/shopping-list/script.js
+++ 06-document-object-model/08-create-item-innerHTML-vs-createElement/shopping-list/script.js
@@ -1,16 +1,34 @@
 //quick
 function createListItem(item){
-
-const list= document.createElement('li')
-list.innerHTML=`
-${item}
-<button class="remove-item btn-link text-red">
-  <i class="fa-solid fa-xmark"></i>
-</button>
-`
-document.querySelector('.items').appendChild(list)
-console.log(list.outerHTML);
-
+  const list=document.querySelector('li');
+  list.innerHTML=`${item}
+  <button class="remove-item btn-link text-red">
+    <i class="fa-solid fa-xmark"></i>
+  </button>`
+ document.querySelector('.items').appendChild(list)
+console.log(list.innerHTML);
 }
 
-createListItem('eggs')
+//createListItem('Eggs')
+
+//better
+
+function createListItem2 (item){
+   const list = document.createElement('li')
+   const text = document.createTextNode(item)
+   const button = document.createElement('button')
+   button.className="remove-item btn-link text-red";
+
+   const icon = document.createElement('i')
+   icon.className="fa-solid fa-xmark";
+   list.appendChild(text)
+   button.appendChild(icon)
+   list.appendChild(button)
+  
+   document.querySelector('.items').appendChild(list)
+   console.log(list);
+
+                                           
+
+}
+createListItem2('Bacon')
