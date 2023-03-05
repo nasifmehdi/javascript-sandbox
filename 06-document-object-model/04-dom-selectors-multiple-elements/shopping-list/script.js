df706b19c3984ff50d42febb598388984946c1a3
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-05 15:09:55 +0600

    my changes

--- 06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script.js
+++ 06-document-object-model/04-dom-selectors-multiple-elements/shopping-list/script.js
@@ -1,44 +1,21 @@
-// querySelectorAll()
-// Returns a NodeList
+console.log(document.querySelector('li:nth-child(2').innerText);
 
-const listItems = document.querySelectorAll('.item');
 
-// Access elements by index
-console.log(listItems[1].innerText);
+let listItems=document.querySelectorAll('li')
+listItems = document.querySelectorAll('.item')
+listItems[0].style.color='orange'
 
-// Setting a color for specific element
-listItems[1].style.color = 'red';
-
-// We can use forEach() on a NodeList
-listItems.forEach((item, index) => {
-  item.style.color = 'red';
-
-  if (index === 1) {
-    item.remove();
-  }
-
-  if (index === 0) {
-    item.innerHTML = ` Oranges
+console.log(listItems[0].innerHTML
+);
+listItems.forEach((element,index) => {
+  if(index == 2)
+   element.remove();
+   //innerText might delete other code
+  if(index ==0){
+    element.innerHTML=`
+    Oranges
     <button class="remove-item btn-link text-red">
       <i class="fa-solid fa-xmark"></i>
-    </button>`;
-  }
-});
-
-// getElementsByClassName()
-// Returns an HTMLCollection
-
-const listItems2 = document.getElementsByClassName('item');
-
-console.log(listItems2[2].innerText);
-
-const listItemsArray = Array.from(listItems2);
-
-listItemsArray.forEach((item) => {
-  console.log(item.innerText);
-});
-
-// getElementsByTagName()
-
-const listItems3 = document.getElementsByTagName('li');
-console.log(listItems3[0].innerText);
+    </button>`
+  } 
+ })
