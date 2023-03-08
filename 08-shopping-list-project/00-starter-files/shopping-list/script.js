1ec0b294fef6abad97e20f4f49b161dc77e03609
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-08 13:37:31 +0600

    add item to shopping list

---
+++ 08-shopping-list-project/00-starter-files/shopping-list/script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('#item-form')
+const formInput= document.querySelector('#item-input')
+const ul=document.getElementById('item-list')
+console.log();
+
+function addItem(e){
+    e.preventDefault()
+    const textInput=formInput.value;
+
+ /*if(formInput.value==''){
+ alert('form is empty')
+ return
+ }
+ */
+ 
+ const li=document.createElement('li')
+ const textNode= document.createTextNode(formInput.value)
+ const button= document.createElement('button')
+
+ button.className= "remove-item btn-link text-red"
+ const icon=document.createElement('i')
+ icon.className= "fa-solid fa-xmark"
+ button.appendChild(icon)
+ li.appendChild(textNode)
+ li.appendChild(button)
+ ul.appendChild(li)
+ console.log(ul);
+}
+form.addEventListener('submit',addItem)
