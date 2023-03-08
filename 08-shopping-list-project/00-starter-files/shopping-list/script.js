1ae8e808fa9b1daafbca962201d6eb287927fb27
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-08 13:41:32 +0600

    comments

--- 08-shopping-list-project/00-starter-files/shopping-list/script.js
+++ 08-shopping-list-project/00-starter-files/shopping-list/script.js
@@ -6,20 +6,23 @@ const ul=document.getElementById('item-list')
 function addItem(e){
     e.preventDefault()
     const textInput=formInput.value;
-
  if(formInput.value==''){
  alert('form is empty')
  return
  }
  
- 
+ //create li,textNode,button,icon
  const li=document.createElement('li')
  const textNode= document.createTextNode(formInput.value)
  const button= document.createElement('button')
+ const icon=document.createElement('i')
 
+//fill in the button,icon
  button.className= "remove-item btn-link text-red"
  const icon=document.createElement('i')
  icon.className= "fa-solid fa-xmark"
+ 
+ //append ul(li(button(icon),textNode))
  button.appendChild(icon)
  li.appendChild(textNode)
  li.appendChild(button)
@@ -27,4 +30,6 @@ function addItem(e){
  formInput.value=''
  console.log(ul);
 }
+
+
 form.addEventListener('submit',addItem)
