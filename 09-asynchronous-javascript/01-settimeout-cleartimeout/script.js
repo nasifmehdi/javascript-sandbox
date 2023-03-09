9d8ac7671c8a48c70187817762e20f4668b21445
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-09 20:10:06 +0600

    setTimeout and clearTimeout

--- 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
+++ 09-asynchronous-javascript/01-settimeout-cleartimeout/script.js
@@ -1,21 +1,9 @@
-// setTimeout - takes in a callback and a time to wait until that callback is executed
-
-setTimeout(function () {
-  console.log('Hello from callback');
-}, 2000);
-
-// Named function
-setTimeout(changeText, 3000);
-
-function changeText() {
-  document.querySelector('h1').textContent = 'Hello from callback';
+const timerId= setTimeout(changeHeader,3000)
+function changeHeader(){
+  document.querySelector('h1').textContent='hello from callback'
 }
-
-// clearTimeout() will clear a timer
-const timerId = setTimeout(changeText, 3000);
-
-document.querySelector('#cancel').addEventListener('click', () => {
-  console.log(timerId);
-  clearTimeout(timerId);
-  console.log('Timer Cancelled');
-});
+const button =document.querySelector('#cancel')
+button.addEventListener('click',() =>{
+  console.log('timer cancelled');
+  clearTimeout(timerId)
+})
