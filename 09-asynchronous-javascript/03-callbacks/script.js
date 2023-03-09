c501c7adb45b981d25d660e00428fe45e076b6cd
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-09 20:59:29 +0600

    callbacks cb()

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -1,29 +1,25 @@
-// function toggle(e) {
-//   e.target.classList.toggle('danger');
-// }
+const posts=[{
+  title:'post one',
+  body:'this is post one'
+},{
+  title:'post two',
+   body:'this is post two'
+}]
 
-// document.querySelector('button').addEventListener('click', toggle);
+function getPosts(){
+  //not setInterval
+  setTimeout(() => { posts.forEach(post => {
+    const div=document.createElement('div')
+    div.innerHTML=`<strong>${post.title} : ${post.body}</strong>`
+    document.querySelector('#posts').appendChild(div)
+  })},1000)
+ 
 
-const posts = [
-  { title: 'Post One', body: 'This is post one' },
-  { title: 'Post Two', body: 'This is post two' },
-];
-
-function createPost(post, cb) {
-  setTimeout(() => {
-    posts.push(post);
-    cb();
-  }, 2000);
 }
-
-function getPosts() {
-  setTimeout(() => {
-    posts.forEach(function (post) {
-      const div = document.createElement('div');
-      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
-      document.querySelector('#posts').appendChild(div);
-    });
-  }, 1000);
+function createPost(post,cb){
+  setTimeout(()=>{posts.push(post)
+    cb()},3000)
+  
 }
-
-createPost({ title: 'Post Three', body: 'This is post' }, getPosts);
+createPost({title:'post three',body:'this is post three'},getPosts)
+//getPosts()
