bfab179c8103520687c582a954eaa897c4610bc1
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-09 22:29:12 +0600

    AJAX and XHR object

--- 09-asynchronous-javascript/04-ajax-xhr/script.js
+++ 09-asynchronous-javascript/04-ajax-xhr/script.js
@@ -1,8 +1,26 @@
-const xhr = new XMLHttpRequest();
+const xhr= new XMLHttpRequest();
 
-// Sepcify method and endpoint/URL
-// xhr.open('GET', './movies.json');
-xhr.open('GET', 'https://api.github.com/users/bradtraversy/repos');
+//xhr.open('GET','./movies.json')
+xhr.open('GET','https://api.github.com/users/nasifmehdi/repos')
+
+xhr.onreadystatechange = function(){
+  if(this.readyState==4 && this.status==200){
+    const data=JSON.parse(this.responseText)
+    data.forEach(repo => {
+      const ul=document.querySelector('ul')
+      const li=document.createElement('li')
+      li.innerHTML=`<strong>${repo.name} : ${repo.description}</strong>`
+      ul.appendChild(li)
+    })
+    };
+  }
+   //console.log(JSON.parse(this.responseText));
+
+xhr.send()
+
+//this.readyState
+//this.status
+//this.responseText
 
 // readyState has 5 possible values
 // - 0: request not initialized
@@ -10,18 +28,4 @@ xhr.open('GET', 'https://api.github.com/users/bradtraversy/repos');
 // - 2: request received
 // - 3: processing request
 // - 4: request finished and response is ready
-xhr.onreadystatechange = function () {
-  if (this.readyState === 4 && this.status === 200) {
-    // console.log(JSON.parse(this.responseText));
-    const data = JSON.parse(this.responseText);
 
-    data.forEach((repo) => {
-      const li = document.createElement('li');
-      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.description}`;
-      document.querySelector('#results').appendChild(li);
-    });
-  }
-};
-
-// Send request
-xhr.send();
