bc1f3da243e7a7da1ed2f0eda1fd3c501e9c84bd
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-09 23:16:36 +0600

    chuck norris joke generator

--- 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
+++ 09-asynchronous-javascript/05-joke-generator-challenge/chuck-joke-generator/script.js
@@ -1,24 +1,25 @@
-const jokeEl = document.getElementById('joke');
-const jokeBtn = document.getElementById('joke-btn');
 
-const generateJoke = () => {
-  const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+const div=document.querySelector('#joke')
+const button=document.querySelector('#joke-btn')
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        // console.log(JSON.parse(this.responseText).value);
-        jokeEl.innerHTML = JSON.parse(this.responseText).value;
-      } else {
-        jokeEl.innerHTML = 'Something Went Wrong (Not Funny)';
-      }
+
+button.addEventListener('click',showJoke)
+document.addEventListener('DOMContentLoaded',showJoke)
+
+function showJoke(){
+  const xhr = new XMLHttpRequest()
+
+xhr.open('GET','https://api.chucknorris.io/jokes/random')
+//xhr.open('GET','https://icanhazdadjoke.com/')
+  xhr.onreadystatechange=function(){
+    if(this.readyState==4 && this.status==200){
+      const data=JSON.parse(this.responseText)
+      div.innerHTML=`${data.value}`
+     
     }
-  };
+  }
+  xhr.send()
+}
 
-  xhr.send();
-};
 
-jokeBtn.addEventListener('click', generateJoke);
-document.addEventListener('DOMContentLoaded', generateJoke);
