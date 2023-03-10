8d761b598336926a58fb3dd6b7140bebc108b978
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-10 17:27:07 +0600

    promises

--- 09-asynchronous-javascript/07-promises/script.js
+++ 09-asynchronous-javascript/07-promises/script.js
@@ -1,31 +1,24 @@
-// Create a promise
-const promise = new Promise((resolve, reject) => {
-  // Do some async task
-  setTimeout(() => {
-    console.log('Async task complete');
-    resolve();
-  }, 1000);
-});
-
-// promise.then(() => {
-//   console.log('Promise consumed..');
-// });
-
-const getUser = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    let error = true;
-
-    if (!error) {
-      resolve({ name: 'John', age: 30 });
-    } else {
-      reject('Error: Something went wrong');
+const promise = new Promise((resolve,reject)=>{
+   setTimeout(() =>{
+    console.log('async task complete');
+    resolve()
+   },1000)
+})
+new Promise((resolve,reject)=>{
+  setTimeout(() =>{
+    let error=false;
+    if(!error){
+      resolve({name:'john',age:30})
     }
-  }, 1000);
-});
-
-getUser
-  .then((user) => console.log(user))
-  .catch((error) => console.log(error))
-  .finally(() => console.log('The promise has been resolved or rejected'));
-
-console.log('Hello from global scope');
+    else
+    reject('something went wrong')
+   console.log('async task complete');
+   
+  },1000)
+}).then((user)=>{console.log(user);})
+.catch(error=>console.log(error))
+.finally(()=>console.log('promised consumed or rejected'))
+promise.then(()=>{
+  console.log('promise consumed');
+})
+//promise(function(resolve,reject){},time)
