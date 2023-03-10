5146df71929fd5f6ad24e11b2902c6c3410eab1f
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-10 19:39:25 +0600

    fetch api

--- 10-fetch-and-async-await/01-fetch-basics/script.js
+++ 10-fetch-and-async-await/01-fetch-basics/script.js
@@ -1,14 +1,9 @@
 // Fetching a JSON file
 fetch('./movies.json')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+.then(response=> {return response.json()})
+.then(data => {console.log(data)})
 
-// Fetching a text file
-fetch('./test.txt')
-  .then((response) => response.text())
-  .then((data) => console.log(data));
+fetch('https://api.github.com/users')
+.then(response=> {return response.json()})
+.then(data => data.forEach(element => console.log(element.login)))
 
-// Fetching from an API
-fetch('https://api.github.com/users/bradtraversy')
-  .then((response) => response.json())
-  .then((data) => (document.querySelector('h1').textContent = data.login));
