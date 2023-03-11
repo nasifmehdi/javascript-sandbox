aadc27001f1216416b40da271ff2933b046a616b
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-11 22:56:13 +0600

    typcode todos mini project

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,81 +1,67 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-const getTodos = () => {
-  fetch(apiUrl + '?_limit=10')
-    .then((res) => res.json())
-    .then((data) => {
-      data.forEach((todo) => addTodoToDOM(todo));
-    });
-};
+const getTodos = () =>{
+  fetch(apiUrl +'?_limit=5')
+  .then( response => response.json())
+  .then(data=>data.forEach(todo => addToDoToDOM(todo)))
+}
+function addToDoToDOM(todo){
+  const div= document.createElement('div')
+    div.appendChild(document.createTextNode(todo.title))
+    
+    div.setAttribute('data-id',todo.id)
+    div.classList.add('todo')
+    if(todo.completed){
+      div.classList.add('done')
+    }
+    document.getElementById('todo-list').appendChild(div)
+}
 
-const addTodoToDOM = (todo) => {
-  const div = document.createElement('div');
-  div.classList.add('todo');
-  div.appendChild(document.createTextNode(todo.title));
-  div.setAttribute('data-id', todo.id);
-
-  if (todo.completed) {
-    div.classList.add('done');
+function createToDos(e){
+e.preventDefault()
+console.log();
+const newTodo ={
+  title: e.target.firstElementChild.value,
+  completed:false
+ }
+ fetch(apiUrl,{
+  method:'POST',
+  body:JSON.stringify(newTodo),
+  headers:{
+    'Content-Type':'application/json'
   }
+ }).then(response => response.json())
+    .then(data => addToDoToDOM(data))
+}
+function toggleCompleted(e){
+  if(e.target.classList.contains('todo'))
+   e.target.classList.toggle('done');
+   updateToDo(e.target.dataset.id,e.target.classList.contains('done'))
+}
+function updateToDo(id,completed){
+  fetch(`${apiUrl}/${id}`,{
+    method:'PUT',
+    body: JSON.stringify({completed}),
+    headers:{
+      'Content-Type':'appilcation/json' 
+    }
+  }).then(response => response.json())
+  .then(data => console.log(data))
+}
+function deleteToDo(e){
+ if(e.target.classList.contains('todo')){
+  const id=e.target.dataset.id
+   fetch(`${apiUrl}/${id}`,{
+    method:"DELETE"
+   }).then(res=>res.json())
+   .then(()=> e.target.remove())
+ }
 
-  document.getElementById('todo-list').appendChild(div);
-};
-
-const createTodo = (e) => {
-  e.preventDefault();
-
-  const newTodo = {
-    title: e.target.firstElementChild.value,
-    completed: false,
-  };
-
-  fetch(apiUrl, {
-    method: 'POST',
-    body: JSON.stringify(newTodo),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => addTodoToDOM(data));
-};
-
-const toggleCompleted = (e) => {
-  if (e.target.classList.contains('todo')) {
-    e.target.classList.toggle('done');
-
-    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
-  }
-};
-
-const updateTodo = (id, completed) => {
-  fetch(`${apiUrl}/${id}`, {
-    method: 'PUT',
-    body: JSON.stringify({ completed }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-};
-
-const deleteTodo = (e) => {
-  if (e.target.classList.contains('todo')) {
-    const id = e.target.dataset.id;
-    fetch(`${apiUrl}/${id}`, {
-      method: 'DELETE',
-    })
-      .then((res) => res.json())
-      .then(() => e.target.remove());
-  }
-};
-
-const init = () => {
-  document.addEventListener('DOMContentLoaded', getTodos);
-  document.querySelector('#todo-form').addEventListener('submit', createTodo);
-  document
-    .querySelector('#todo-list')
-    .addEventListener('click', toggleCompleted);
-  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
-};
-
-init();
+}
+function init(){
+document.addEventListener('DOMContentLoaded',getTodos)
+document.querySelector('#todo-form').addEventListener('submit',createToDos)
+document.querySelector('#todo-list').addEventListener('click',toggleCompleted)
+document.querySelector('#todo-list').addEventListener('dblclick',deleteToDo)
+}
+init()
