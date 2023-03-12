6f3152f6d4697759fe8085c75b3a92939681db50
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-12 16:21:46 +0600

    show hide spinner and add popular tv shows

--- 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
+++ 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
@@ -35,15 +35,58 @@ results.forEach(movie =>{
 document.querySelector('#popular-movies').appendChild(div)
 })
 }
+async function displayPouplarTVShows(){
+  const {results}= await fetchAPIData('/tv/popular')
+console.log(results);
+results.forEach(tv =>{
+  const div= document.createElement('div')
+  div.className='card-body card'
+  div.innerHTML=`<a href="tv-details.html?id=${tv.id}">
+  ${
+    tv.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w300${tv.poster_path}"
+    class="card-img-top"
+    alt="${tv.name}"
+  />`
+      : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${tv.name}"
+/>`
+  }
+</a>
+<div class="card-body">
+  <h5 class="card-title">${tv.name}</h5>
+  <p class="card-text">
+    <small class="text-muted">Release: ${tv.first_air_date}</small>
+    
+  </p>
+  <p><small class="text-muted">Country: ${tv.origin_country}</small></p>
+</div>`
+
+document.querySelector('#popular-shows').appendChild(div)
+})
+}
 
 async function fetchAPIData(endpoint){
 
   const API_key='6e24d641b14f53e559c6bc6710b05362'
   const API_url='https://api.themoviedb.org/3/'
-
+  showSpinner()
   const response=await fetch(`${API_url}${endpoint}?api_key=${API_key}&language=en-US`)
   const data=await response.json()
+  hideSpinner()
   return data;
+}
+
+function showSpinner(){
+document.querySelector('.spinner').classList.add('show')
+ 
+}
+function hideSpinner(id){
+  document.querySelector('.spinner').classList.remove('show')
+  
 }
 
 
@@ -65,9 +108,11 @@ function init(){
     break;
     case '/tv-details.html':
     console.log('tv details');
+    
     break;
     case '/shows.html':
-    console.log('shows');
+    //console.log('shows');
+    displayPouplarTVShows()
     break;
 
 
