97eb068de931f08bb2c455bba44b778329983187
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-12 18:06:29 +0600

    display movie details from api

--- 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
+++ 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
@@ -35,6 +35,67 @@ results.forEach(movie =>{
 document.querySelector('#popular-movies').appendChild(div)
 })
 }
+
+async function displayMovieDetails(){
+
+const movieID=window.location.search.split('=')[1]
+console.log(movieID); 
+const movie=await fetchAPIData(`/movie/${movieID}`)
+const div= document.createElement('div')
+div.innerHTML=`<div id="movie-details">
+<div class="details-top">
+  <div>
+   ${
+    movie.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w300${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+/>`
+   }
+  </div>
+  <div>
+    <h2>${movie.title}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${movie.vote_average.toFixed(1)}/10
+    </p>
+    <p class="text-muted">Release Date: ${movie.release_date}</p>
+    
+    <p>
+      ${movie.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+     ${movie.genres.map((genre)=>
+      `<li>${genre.name}</li>`).join('')
+     }
+      
+    </ul>
+    <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span>  $ ${addCommastoNumber(movie.budget)}</li>
+    <li><span class="text-secondary">Revenue:</span> $ ${addCommastoNumber(movie.revenue)}</li>
+    <li><span class="text-secondary">Runtime:</span>   ${movie.runtime} minutes</li>
+    <li><span class="text-secondary">Status:</span>    ${movie.status} </li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+  ${movie.production_companies.map((company)=>`<span>${company.name}</span><br>`).join(' ')}</div>
+</div>
+</div>`
+document.querySelector('#movie-details').appendChild(div)
+
+}
 async function displayPouplarTVShows(){
   const {results}= await fetchAPIData('/tv/popular')
 console.log(results);
@@ -101,7 +162,8 @@ function init(){
     //console.log('home');
     break;
     case '/movie-details.html':  
-    console.log('movie details');
+    //console.log('movie details');
+    displayMovieDetails()
     break;
     case '/search.html':
     console.log('search');
@@ -130,6 +192,9 @@ pageLinks.forEach(link=>{
    
 })
 
+}
+function addCommastoNumber(x){
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 document.addEventListener('DOMContentLoaded',init)
