85d999661f1aa2a86b6b890d8fb3efdb17d4caaf
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-12 15:54:52 +0600

    display popular movies on flixx app

--- 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
+++ 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
@@ -1,90 +1,90 @@
 const global = {
   currentPage: window.location.pathname,
-};
-
-// Display 20 most popular movies
-async function displayPopularMovies() {
-  const { results } = await fetchAPIData('movie/popular');
-
-  results.forEach((movie) => {
-    const div = document.createElement('div');
-    div.classList.add('card');
-    div.innerHTML = `
-          <a href="movie-details.html?id=${movie.id}">
-            ${
-              movie.poster_path
-                ? `<img
-              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
-              class="card-img-top"
-              alt="${movie.title}"
-            />`
-                : `<img
-            src="../images/no-image.jpg"
-            class="card-img-top"
-            alt="${movie.title}"
-          />`
-            }
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">${movie.title}</h5>
-            <p class="card-text">
-              <small class="text-muted">Release: ${movie.release_date}</small>
-            </p>
-          </div>
-        `;
-
-    document.querySelector('#popular-movies').appendChild(div);
-  });
 }
 
-// Fetch data from TMDB API
-async function fetchAPIData(endpoint) {
-  // Register your key at https://www.themoviedb.org/settings/api and enter here
-  // Only use this for development or very small projects. You should store your key and make requests from a server
-  const API_KEY = '';
-  const API_URL = 'https://api.themoviedb.org/3/';
+async function displayPopularMovies(){
+//const results= await fetchAPIData('/movie/popular')
+//destructed {results}
+const {results}= await fetchAPIData('/movie/popular')
+console.log(results);
+results.forEach(movie =>{
+  const div= document.createElement('div')
+  div.className='card-body card'
+  div.innerHTML=`<a href="movie-details.html?id=${movie.id}">
+  ${
+    movie.poster_path
+      ? `<img
+    src="https://image.tmdb.org/t/p/w300${movie.poster_path}"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+      : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+/>`
+  }
+</a>
+<div class="card-body">
+  <h5 class="card-title">${movie.title}</h5>
+  <p class="card-text">
+    <small class="text-muted">Release: ${movie.release_date}</small>
+  </p>
+</div>`
 
-  const response = await fetch(
-    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
-  );
+document.querySelector('#popular-movies').appendChild(div)
+})
+}
 
-  const data = await response.json();
+async function fetchAPIData(endpoint){
 
+  const API_key='6e24d641b14f53e559c6bc6710b05362'
+  const API_url='https://api.themoviedb.org/3/'
+
+  const response=await fetch(`${API_url}${endpoint}?api_key=${API_key}&language=en-US`)
+  const data=await response.json()
   return data;
 }
 
-// Highlight active link
-function highlightActiveLink() {
-  const links = document.querySelectorAll('.nav-link');
-  links.forEach((link) => {
-    if (link.getAttribute('href') === global.currentPage) {
-      link.classList.add('active');
-    }
-  });
-}
 
-// Init App
-function init() {
-  switch (global.currentPage) {
+
+
+function init(){
+
+  switch(global.currentPage){
     case '/':
     case '/index.html':
-      displayPopularMovies();
-      break;
-    case '/shows.html':
-      console.log('Shows');
-      break;
-    case '/movie-details.html':
-      console.log('Movie Details');
-      break;
-    case '/tv-details.html':
-      console.log('TV Details');
-      break;
+    displayPopularMovies()
+    //console.log('home');
+    break;
+    case '/movie-details.html':  
+    console.log('movie details');
+    break;
     case '/search.html':
-      console.log('Search');
-      break;
+    console.log('search');
+    break;
+    case '/tv-details.html':
+    console.log('tv details');
+    break;
+    case '/shows.html':
+    console.log('shows');
+    break;
+
+
   }
 
-  highlightActiveLink();
+highlightPageLinks()
 }
 
-document.addEventListener('DOMContentLoaded', init);
+function highlightPageLinks(){
+
+const pageLinks= document.querySelectorAll('.nav-link')
+pageLinks.forEach(link=>{
+  if(link.getAttribute('href')==global.currentPage)
+   link.classList.add('active')
+   
+})
+
+}
+
+document.addEventListener('DOMContentLoaded',init)
