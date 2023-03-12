cc00ba8bac7347d58808321ea83248938890f868
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-12 18:30:57 +0600

    add background image

--- 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
+++ 11-flixx-app-project/03-display-popular-movies/flixx-app/js/script.js
@@ -41,6 +41,7 @@ async function displayMovieDetails(){
 const movieID=window.location.search.split('=')[1]
 console.log(movieID); 
 const movie=await fetchAPIData(`/movie/${movieID}`)
+displayBackgroundImage('movie', movie.backdrop_path);
 const div= document.createElement('div')
 div.innerHTML=`<div id="movie-details">
 <div class="details-top">
@@ -192,6 +193,26 @@ pageLinks.forEach(link=>{
    
 })
 
+}
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.1';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 function addCommastoNumber(x){
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
