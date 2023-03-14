a8878c6c4fd7b2b2c95adf1bb0be001603ab07e3
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-14 17:49:52 +0600

    animated clock part 1

--- 12-web-browser-apis/05-animated-clock-1/animated-clock/script.js
+++ 12-web-browser-apis/05-animated-clock-1/animated-clock/script.js
@@ -1,104 +1,116 @@
-function clock() {
-  const now = new Date();
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
+function clock(){
+const now= new Date();
+const canvas= document.querySelector('#canvas')
+const ctx=canvas.getContext('2d')
 
-  // Setup canvas
-  ctx.save(); // save the default state
-  ctx.clearRect(0, 0, 500, 500);
-  ctx.translate(250, 250); // Put 0,0 in the middle
-  ctx.rotate(-Math.PI / 2); // Rotate clock -90deg
+ctx.save()
+ctx.clearRect(0,0,500,500)
+ctx.translate(250,250)
+ctx.rotate(-Math.PI/2)
 
-  // Set default styles
-  ctx.strokeStyle = '#000000';
-  ctx.fillStyle = '#f4f4f4';
-  ctx.lineWidth = 5;
-  ctx.lineCap = 'round';
+//default style
+ctx.strokeStyle='#000000'
+ctx.fillStyle='#f4f4'
+ctx.lineWidth=5
+ctx.lineCap='round'
 
-  // Draw clock face/border
-  ctx.save();
-  ctx.beginPath();
-  ctx.lineWidth = 14;
-  ctx.strokeStyle = '#800000';
-  ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
-  ctx.stroke();
-  ctx.fill();
-  ctx.restore();
+//draw clock face
+ctx.save()
+ctx.beginPath()
+ctx.lineWidth=14
+ctx.strokeStyle='#800000'
+ctx.arc(0,0,150,0,Math.PI*2,true)
+ctx.stroke()
+ctx.fill()
+ctx.restore()
 
-  // Draw hour lines
-  ctx.save();
-  for (let i = 0; i < 12; i++) {
-    ctx.beginPath();
-    ctx.rotate(Math.PI / 6);
-    ctx.moveTo(100, 0);
-    ctx.lineTo(120, 0);
-    ctx.stroke();
-  }
-  ctx.restore();
-
-  // Draw minute lines
-  ctx.save();
-  ctx.lineWidth = 4;
-  for (let i = 0; i < 60; i++) {
-    if (i % 5 !== 0) {
-      ctx.beginPath();
-      ctx.moveTo(117, 0);
-      ctx.lineTo(120, 0);
-      ctx.stroke();
+//draw hour lines
+ctx.save()
+let i=0
+while(i<12){
+  ctx.beginPath()
+  ctx.rotate(Math.PI/6)
+  ctx.moveTo(100,0)
+  ctx.lineTo(120,0)
+  ctx.stroke()
+  i++
+}
+ctx.restore()
+//draw minute lines
+ctx.save()
+let j=0
+ctx.lineWidth=4
+while(j<60){
+  if(j % 5 !=0){  
+    ctx.beginPath()
+    ctx.moveTo(130,0)
+    ctx.lineTo(120,0)
+    ctx.stroke()
     }
-    ctx.rotate(Math.PI / 30);
-  }
-  ctx.restore();
+    ctx.rotate(Math.PI/30)
+    j++
+}
+ctx.restore()
+//draw hour hand
+const hr= now.getHours()%12
+const min=now.getMinutes()
+const sec=now.getSeconds()
+console.log(`${hr}:${min}:${sec}`);
 
-  // Get current time
-  const hr = now.getHours() % 12;
-  const min = now.getMinutes();
-  const sec = now.getSeconds();
 
-  // console.log(`${hr}:${min}:${sec}`);
+ctx.save()
+ctx.rotate(
+  (Math.PI/6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+)
+ctx.strokeStyle='#800000'
+ctx.lineWidth= 14;
+ctx.beginPath()
+ctx.moveTo(-20,0)
+ctx.lineTo(80,0)
+ctx.stroke()
+ctx.restore()
 
-  // Draw hour hand
-  ctx.save();
-  ctx.rotate(
-    (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
-  );
-  ctx.strokeStyle = '#800000';
-  ctx.lineWidth = 14;
-  ctx.beginPath();
-  ctx.moveTo(-20, 0);
-  ctx.lineTo(80, 0);
-  ctx.stroke();
-  ctx.restore();
+//draw minute hand
+ctx.save()
+ctx.rotate(
+  (Math.PI/30) * min + (Math.PI / 1800) * sec 
+)
+ctx.strokeStyle='orange'
+ctx.lineWidth= 12;
+ctx.beginPath()
+ctx.moveTo(-24,0)
+ctx.lineTo(100,0)
+ctx.stroke()
+ctx.restore()
 
-  // Draw min hand
-  ctx.save();
-  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
-  ctx.strokeStyle = '#800000';
-  ctx.lineWidth = 10;
-  ctx.beginPath();
-  ctx.moveTo(-28, 0);
-  ctx.lineTo(112, 0);
-  ctx.stroke();
-  ctx.restore();
 
-  // Draw sec hand
-  ctx.save();
-  ctx.rotate((sec * Math.PI) / 30);
-  ctx.strokeStyle = '#FF7F50';
-  ctx.fillStyle = '#FF7F50';
-  ctx.lineWidth = 6;
-  ctx.beginPath();
-  ctx.moveTo(-30, 0);
-  ctx.lineTo(100, 0);
-  ctx.stroke();
-  ctx.beginPath();
-  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
-  ctx.fill();
-  ctx.restore();
+//draw second hand
 
-  ctx.restore(); // restore default state
 
-  requestAnimationFrame(clock);
+ctx.save()
+ctx.rotate(
+  sec * Math.PI / 30
+)
+ctx.strokeStyle='white'
+ctx.lineWidth= 2;
+ctx.fillStyle='white'
+ctx.beginPath()
+ctx.moveTo(-21,0)
+ctx.lineTo(138,0)
+ctx.stroke()
+ctx.beginPath()
+ctx.arc(0,0,10,0,Math.PI * 2,true)
+ctx.fill()
+ctx.restore()
+
+
+
+ctx.restore()
+requestAnimationFrame(clock)
 }
 
-requestAnimationFrame(clock);
+function init(){
+  
+  requestAnimationFrame(clock)
+}
+init()
