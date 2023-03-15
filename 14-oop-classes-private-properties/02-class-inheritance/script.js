37168b806691816080499d1760b619965ee20dbf
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-16 00:46:04 +0600

    class inheritance

--- 14-oop-classes-private-properties/02-class-inheritance/script.js
+++ 14-oop-classes-private-properties/02-class-inheritance/script.js
@@ -1,45 +1,33 @@
-// Parent class
-class Shape {
-  constructor(name) {
-    this.name = name;
+class Shape{
+ 
+  constructor(name){
+    this.name=name
   }
-
-  logName() {
-    console.log('Shape Name: ' + this.name);
+  name(){
+    return this.name
   }
-}
-
-// Rectangle - Sub class
-class Rectangle extends Shape {
-  constructor(name, width, height) {
-    super(name);
-
-    this.width = width;
-    this.height = height;
+  logName(){
+    console.log(this.name);
   }
 }
 
-// Circle - Sub class
-class Circle extends Shape {
-  constructor(name, radius) {
-    super(name);
-
-    this.radius = radius;
+class Rectangle extends Shape{
+   constructor(name,height,width){
+    super(name)
+    this.height=height
+    this.width=width
+   }
+  area(){
+    return this.height * this.width
   }
-
-  // We can override the Shape logName() (polymorphism)
-  logName() {
-    console.log('Circle Name: ' + this.name);
+  isSquare(){
+    return this.height == this.width
+  }
+  logName(){
+    console.log('inheritence' + this.name);
   }
 }
 
-const rect = new Rectangle('Rect 1', 20, 20);
-console.log(rect);
-rect.logName();
-
-const cir = new Circle('Cir 1', 30);
-cir.logName();
-
-// rect is an instance of both Rectangle and Shape
-console.log(rect instanceof Rectangle);
-console.log(rect instanceof Shape);
+const rect= new Rectangle('rerw',40,50)
+const shape= new Shape('shasfas')
+console.log(rect.logName());
