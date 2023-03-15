58c7bbbd25adf306cb2ca6447ec93cfb792e9406
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-15 20:24:23 +0600

    prototype

--- 13-oop-constructors-prototypes/05-prototypes/script.js
+++ 13-oop-constructors-prototypes/05-prototypes/script.js
@@ -9,8 +9,4 @@ function Rectangle(name, width, height) {
 
 const rect = new Rectangle('Rect', 10, 10);
 
-//  Rectangle.prototype inherits from Object.prototype. That's why we can use toString(), etc
-console.log(rect.toString());
-
-// To get the prototype of an object
 console.log(Object.getPrototypeOf(rect));
