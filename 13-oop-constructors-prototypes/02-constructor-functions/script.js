be8eff612d66b99acaaf8a115f3bdb63d2a7fbc7
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-15 19:46:55 +0600

    boxing unboxing function and number obj

--- 13-oop-constructors-prototypes/02-constructor-functions/script.js
+++ 13-oop-constructors-prototypes/02-constructor-functions/script.js
@@ -9,3 +9,14 @@ const rect= new rectangle(20,30)
 console.log(rect.area());
 console.log(rect instanceof rectangle);
 console.log(rect.constructor);
+
+const string='hello'
+const stringObj= new String('hello')
+
+console.log(string.toUpperCase(), typeof string);
+console.log(stringObj.toUpperCase(), typeof stringObj);
+
+const num = new Number('34')
+console.log(num);
+const fun = new Function('x', 'console.log(x)')
+fun(2)
