73f34651e342dc15c252f1978d8efa38aee57b3d
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-16 16:19:49 +0600

    private property convention

--- 14-oop-classes-private-properties/07-private-properties-convention/script.js
+++ 14-oop-classes-private-properties/07-private-properties-convention/script.js
@@ -1,53 +1,43 @@
-class Wallet {
-  constructor() {
-    this._balance = 0;
-    this._transactions = [];
-  }
-
-  deposit(amount) {
-    this._processDeposit(amount);
-    this._balance += amount;
-  }
-
-  withdraw(amount) {
-    if (amount > this._balance) {
-      console.log('Not enough funds');
-      return;
+class Wallet{
+ constructor(){
+    this._balance=0
+    this._transactions=[]
+ }
+ deposit(amount){
+  this._balance=this._balance+amount
+  this._transactions.push({
+    _type:'deposit',
+    _amount:amount
+ })
+  console.log('deposited: '+amount);
+  
+ }
+ withdraw(amount){
+    if(this._balance < amount){
+        console.log(amount +' is more than balance' );
+        return;
     }
-
-    this._processWithdraw(amount);
-    this._balance -= amount;
-  }
-
-  _processDeposit(amount) {
-    console.log(`Depositing ${amount}`);
-
+    
+    this._balance=this._balance-amount
     this._transactions.push({
-      type: 'deposit',
-      amount,
-    });
-  }
-
-  _processWithdraw(amount) {
-    console.log(`Withdrawing ${amount}`);
-
-    this._transactions.push({
-      type: 'withdraw',
-      amount,
-    });
-  }
-
-  get balance() {
-    return this._balance;
-  }
-
-  get transactions() {
-    return this._transactions;
-  }
+        _type:'withdrawn',
+        _amount:amount
+     })
+    console.log('withdrawn: '+amount);
+ 
+ }
+ get balance(){
+    return 'balance:'+this._balance
+ }
+ get transactions(){
+    return this._transactions
+ }
+ 
 }
 
-const wallet = new Wallet();
-wallet.deposit(300);
-wallet.withdraw(50);
+const wallet=new Wallet()
+console.log(wallet.deposit(500));
+console.log(wallet.balance);
+console.log(wallet.withdraw(200));
 console.log(wallet.balance);
 console.log(wallet.transactions);
